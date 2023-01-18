16b10c23f9f4dcbe78f8b4bce222b8306824e1f7
kabundege <christophekwizera1@gmail.com>
2023-01-18 22:52:13 +0200

    report

--- src/pages/service/page.tsx
+++ src/pages/service/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useContext, useEffect, useState } from "react";
+import { AiFillCheckCircle, AiOutlineCheckCircle } from "react-icons/ai";
 import Button from "../../Components/Button";
 import SelectField from "../../Components/SelectField";
 import UploadField from "../../Components/UploadField";
+import { hoverFade } from "../../constants/comon.styles";
 import { StoreContext } from "../../context";
 import { Farm } from "../farms/page";
 
@@ -10,6 +12,8 @@ export default function Services() {
   const {user} = useContext(StoreContext)
   const [file, setFile] = useState();
   const [farms, setFarm] = useState<Farm[]>([]);
+  const [result, setResult] = useState<string>();
+  const [isSick, setSick] = useState(false);
   const [type, setType] = useState<"image" | "video">("image");
 
   useEffect(()=>{
@@ -32,6 +36,10 @@ export default function Services() {
     setType((prev) => (prev === "image" ? "video" : "image"));
   };
 
+  const toggleSick = () => {
+    setSick(prev => !prev)
+  }
+
   const Select = (
     <div className="w-1/2 relative translate-y-3">
       <SelectField
@@ -41,6 +49,10 @@ export default function Services() {
       />
     </div>
   )
+
+  const toggleResult = (link?:string) => {
+    setResult(link)
+  }
 
   const viewCase = () => {
     switch (type) {
@@ -73,7 +85,7 @@ export default function Services() {
                 className="rounded-none"
                 accept="image/*, video/*"
               />
-              <Button text="Submit" className="mt-5 w-3/12" />
+              <Button onClick={()=>toggleResult('https://picsum.photos/200/300.webp')} text="Submit" className="mt-5 w-3/12" />
             </div>
           </>
         );
@@ -110,7 +122,31 @@ export default function Services() {
         </div>
         {Select}
       </div>
-      {viewCase()}
+      { 
+        result? 
+          (
+            <div className="w-full flex flex-col items-center">
+              <h2 className="text-center text-lg mx-auto my-5 text-gray-400 font-medium">
+                Welcome to our service! Here, you can enable real-time video stream and our AI model will predict whether the chicken is sick or not. To do this, simply click on the "Enable Video Stream" button and the video stream will start. The AI model will then process the video frames and return the predictions in real-time. You will be able to see the prediction, with an indication of whether the chicken is sick or not. Thank you for using our service!
+              </h2>
+              <img alt="" src={result} className="w-full bg-bash h-96 object-contain" />
+              <div className="flex justify-between items-center mt-5 w-full">
+                <div className="flex border p-2">
+                  <p onClick={toggleSick} className={"flex items-center whitespace-nowrap mr-2 m-1"+hoverFade}>
+                    { isSick ? <AiOutlineCheckCircle className="text-borange" /> : <AiFillCheckCircle className="text-borange" />}
+                    <span className="ml-2">Found Sick</span>
+                  </p>
+                  <p onClick={toggleSick} className={"flex items-center whitespace-nowrap m-1"+hoverFade}>
+                    { !isSick ? <AiOutlineCheckCircle className="text-borange" /> : <AiFillCheckCircle className="text-borange" />}
+                    <span className="ml-2">Found Healthy</span>
+                  </p>
+                </div>
+                <Button text="Save"  onClick={()=>toggleResult()}  className="w-1/3 ml-5" />
+              </div>
+            </div> 
+          ) : 
+          viewCase()
+      }
     </div>
   );
 }
